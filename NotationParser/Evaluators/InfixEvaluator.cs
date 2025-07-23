c60e412b55e5ad8b137f2cbb10bb1577077869ed
GoodwillMonoto <gkmono2@gmail.com>
2025-07-23 17:43:56 +0200

    initialise handlers

--- NotationParser/Evaluators/InfixEvaluator.cs
+++ NotationParser/Evaluators/InfixEvaluator.cs
@@ -21,7 +21,6 @@ namespace MathNotationParser.Evaluators
             { '(', new MathOperator('(', 4) }
         };
 
-
         public InfixEvaluator(string expression)
         {
             Expression = expression;
@@ -76,30 +75,32 @@ namespace MathNotationParser.Evaluators
         private void HandleBrackets()
         {
           var bracketHandler = new BracketHandlerCommand(expression: Expression);
+          bracketHandler.Handle();
+          
         }
 
         private void HandleDivision()
         {
-            // Logic to handle division in the expression
-            // This will involve finding division operators and evaluating them
+            var divisionHandler = new DivisionHandlerCommand(expression: Expression);
+            divisionHandler.Handle();
         }
 
         private void HandleMultiplication()
         {
-            // Logic to handle multiplication in the expression
-            // This will involve finding multiplication operators and evaluating them
+            var multiplicationHandler = new MultiplicationHandlerCommand(expression: Expression);
+            multiplicationHandler.Handle();
         }
 
         private void HandleAddition()
         {
-            // Logic to handle addition in the expression
-            // This will involve finding addition operators and evaluating them
+            var additionHandler = new AdditionHandlerCommand(expression: Expression);
+            additionHandler.Handle();
         }
 
         private void HandleSubtraction()
         {
-            // Logic to handle subtraction in the expression
-            // This will involve finding subtraction operators and evaluating them
+            var subtractionHandler = new SubtractionHandlerCommand(expression: Expression);
+            subtractionHandler.Handle();
         }
     }
 }
