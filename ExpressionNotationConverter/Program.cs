6b95bb336d3489d029bb122be8ace5798290038c
Goodwill Monoto <goodwill.monoto@dariel.co.za>
2025-07-29 12:29:25 +0200

    update to evaluate after conversion

--- ExpressionNotationConverter/Program.cs
+++ ExpressionNotationConverter/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using MathNotationParser.Evaluators;
 using MathNotationParser.NotationParsers;
-PostfixToInfixParser parser = new PostfixToInfixParser();
+PostfixToInfixParser parser = new PostfixToInfixParser(new InfixEvaluator());
 Console.WriteLine("This is a simple console application to convert expression notations:");
 Console.WriteLine("It supports postfix to infix notatation");
 Console.WriteLine("Please ensure you have atleast one charater of space between numbers an oporators");
@@ -11,6 +12,12 @@ string input = Console.ReadLine();
 var result = parser.ToInfix(input);
 Console.WriteLine("The converted infix expression is:");
 Console.WriteLine(result);
+Console.WriteLine("Press any key to Continue...");
+Console.ReadLine();
+Console.WriteLine("Evaluated result:");
+Console.WriteLine(parser.Evaluate());
+Console.WriteLine("Press any key to exit...");
+Console.ReadLine();
 
 
 
